01be8b531089097941efd32bcdcf9ab1962de10c
Sravani Vedam <sravanivedam@zipcoders-MacBook-Pro.local>
2019-04-04 17:45:26 -0400

    Fixed package name

--- src/main/java/com/example/PhoenixVideoLibrary/Controllers/VideoController.java
+++ src/main/java/com/example/PhoenixVideoLibrary/Controllers/VideoController.java
@@ -1,4 +1,4 @@
-package com.example.demo.Controllers;
+package com.example.PhoenixVideoLibrary.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/example/PhoenixVideoLibrary/DemoApplication.java
+++ src/main/java/com/example/PhoenixVideoLibrary/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.example.PhoenixVideoLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
