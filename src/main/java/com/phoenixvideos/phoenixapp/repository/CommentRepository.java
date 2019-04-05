1b107927fb78b73b22b3dcc007441f359ac72a10
Aswathy Kanakarajan <aswathykanakarajan@zipcoders-MacBook-Pro-7.local>
2019-04-05 16:38:38 -0400

    Initial project structure updated - final

--- src/main/java/com/phoenixvideos/phoenixapp/repository/CommentRepository.java
+++ src/main/java/com/phoenixvideos/phoenixapp/repository/CommentRepository.java
@@ -1,6 +1,7 @@
 package com.phoenixvideos.phoenixapp.repository;
 
+import com.phoenixvideos.phoenixapp.model.Comment;
 import org.springframework.data.repository.CrudRepository;
 
-public interface CommentRepository extends CrudRepository<CommentRepository, Long> {
+public interface CommentRepository extends CrudRepository<Comment, Long> {
 }
