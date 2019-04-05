4c6d525ab2d29f47d6a604724b9191e917399219
Sravani Vedam <sravanivedam@zipcoders-MacBook-Pro.local>
2019-04-05 10:06:00 -0400

    Changed folder name

--- src/main/java/com/videos/PhoenixVideoLibrary/Controllers/VideoController.java
+++ src/main/java/com/videos/PhoenixVideoLibrary/Controllers/VideoController.java
@@ -1,4 +1,4 @@
-package com.example.PhoenixVideoLibrary.Controllers;
+package com.videos.PhoenixVideoLibrary.Controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
--- src/main/java/com/videos/PhoenixVideoLibrary/DemoApplication.java
+++ src/main/java/com/videos/PhoenixVideoLibrary/DemoApplication.java
@@ -1,4 +1,4 @@
-package com.example.PhoenixVideoLibrary;
+package com.videos.PhoenixVideoLibrary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
