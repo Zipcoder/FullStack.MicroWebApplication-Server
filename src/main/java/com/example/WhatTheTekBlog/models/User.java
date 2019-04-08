9c9bb4387a4c5dbe1a654f968b6bef85674b4605
Eleonor Bart <eleonorbart@zipcoders-MacBook-Pro-51.local>
2019-04-08 15:21:33 -0400

    Added Users. Tested models

--- src/main/java/com/example/WhatTheTekBlog/models/User.java
+++ src/main/java/com/example/WhatTheTekBlog/models/User.java
@@ -1,4 +1,67 @@
 package com.example.WhatTheTekBlog.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import java.util.Set;
+
+@Entity
 public class User {
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String name;
+    private String email;
+    @OneToMany(mappedBy = "creator")
+    @JsonIgnore
+    private Set<Post> posts;
+    @OneToMany(mappedBy = "commenter")
+    @JsonIgnore
+    private Set<Comments> comments;
+
+    public User() {
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public Set<Post> getPosts() {
+        return posts;
+    }
+
+    public void setPosts(Set<Post> posts) {
+        this.posts = posts;
+    }
+
+    public Set<Comments> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comments> comments) {
+        this.comments = comments;
+    }
 }
---
+++ src/test/java/com/example/WhatTheTekBlog/services/UserServiceTest.java
@@ -0,0 +1,51 @@
+package com.example.WhatTheTekBlog.services;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+public class UserServiceTest {
+
+    @Autowired
+    private UserService userService;
+
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+
+    @Test
+    public void findAllUsers() {
+    }
+
+    @Test
+    public void findById() {
+    }
+
+    @Test
+    public void getPostsByUser() {
+    }
+
+    @Test
+    public void getCommentsByUser() {
+    }
+
+    @Test
+    public void create() {
+    }
+
+    @Test
+    public void delete() {
+    }
+
+    @Test
+    public void update() {
+    }
+}
