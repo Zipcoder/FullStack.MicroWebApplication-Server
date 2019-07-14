7b3f3e7bc1e59fc1062adc0874c990df14d3f90d
Henry <cataniahf@gmail.com>
2019-07-14 10:27:13 -0400

    working on the angular stuff...   no progress as yet

--- money-app/src/app/app.module.ts
+++ money-app/src/app/app.module.ts
@@ -3,17 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccountService } from './account.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AccountCreatorComponent } from './account-creator/account-creator.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    routingComponents
+    routingComponents,
+    AccountCreatorComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule
   ],
-  providers: [],
+  providers: [AccountService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
