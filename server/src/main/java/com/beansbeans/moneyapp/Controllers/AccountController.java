59f60176444708566e3b2a6243fa3b5ae9c13a7d
Henry <cataniahf@gmail.com>
2019-07-14 14:13:52 -0400

    Account controller and service built out

--- server/src/main/java/com/beansbeans/moneyapp/Controllers/AccountController.java
+++ server/src/main/java/com/beansbeans/moneyapp/Controllers/AccountController.java
@@ -18,10 +18,24 @@ public class AccountController {
         return new ResponseEntity<>(accountService.create(account), HttpStatus.CREATED);
     }
 
+    @GetMapping("/accounts")
+    public ResponseEntity<Iterable<Account>> findAllAccounts(){
+        return new ResponseEntity<>(accountService.getAllAccounts(), HttpStatus.OK);
+    }
+
     @GetMapping("/accounts/{id}")
     public ResponseEntity<Account> getById(@PathVariable Long id){
         return new ResponseEntity<>(accountService.show(id), HttpStatus.OK);
     }
 
+    @PostMapping("/accounts/{id}")
+    public ResponseEntity<Account> updateBalance(@PathVariable Long id, @RequestBody Account account){
+        return new ResponseEntity<>(accountService.updateBalance(id, account), HttpStatus.OK);
+    }
+
+    @DeleteMapping("/accounts/{id}")
+    public ResponseEntity<Boolean> deleteAccount(@PathVariable Long id){
+        return new ResponseEntity<>(accountService.deleteAccount(id), HttpStatus.OK);
+    }
 
 }
