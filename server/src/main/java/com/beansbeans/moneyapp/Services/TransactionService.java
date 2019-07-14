15ae09ac76a713f025afb71aa80bd8dfb5cb0306
Henry <cataniahf@gmail.com>
2019-07-14 14:57:13 -0400

    Fleshed out transaction services

--- server/src/main/java/com/beansbeans/moneyapp/Services/TransactionService.java
+++ server/src/main/java/com/beansbeans/moneyapp/Services/TransactionService.java
@@ -1,15 +1,19 @@
 package com.beansbeans.moneyapp.Services;
 
+import com.beansbeans.moneyapp.Model.Account;
 import com.beansbeans.moneyapp.Model.Transaction;
 import com.beansbeans.moneyapp.Repositories.TransactionRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import com.beansbeans.moneyapp.Repositories.AccountRepository;
 
 import javax.transaction.Transactional;
 
 @Service
 @Transactional
 public class TransactionService {
+    private AccountRepository accountRepository;
+
     @Autowired
     private TransactionRepository transactionRepository;
 
@@ -25,9 +29,33 @@ public class TransactionService {
         return transactionRepository.findById(transactionId).get();
     }
 
-//    public Transaction update(Long transactionId, Transaction newTransaction){
-//        Transaction originalTransaction = transactionRepository.findById(transactionId).get();
-//        originalTransaction.setAmount(newTransaction.getAmount());
-//        originalTransaction.s
-//    }
+    public void depositTo(Long id, Double amount){
+        Account account = accountRepository.findById(id).get();
+        Double initialBalance = account.getBalance();
+        account.setBalance(initialBalance + amount);
+        accountRepository.save(account);
+    }
+
+    public void withdrawFrom(Long id, Double amount){
+        Account account = accountRepository.findById(id).get();
+        Double initialBalance = account.getBalance();
+        if((initialBalance - amount) < 0.0){
+            throw new IllegalArgumentException("Unable to complete Transaction.  Insufficient Funds");
+        }
+        account.setBalance(initialBalance - amount);
+        accountRepository.save(account);
+    }
+
+    public void transferFunds(Long fromId, Long toId, Double amount){
+        Account fromAccount = accountRepository.findById(fromId).get();
+        Account toAccount = accountRepository.findById(toId).get();
+        Double initalBalance = fromAccount.getBalance();
+        if((initalBalance - amount) < 0.00){
+            throw new IllegalArgumentException("Unable to complete Transaction.  Insufficient Funds");
+        }
+        fromAccount.setBalance(initalBalance - amount);
+        toAccount.setBalance(initalBalance + amount);
+        accountRepository.save(fromAccount);
+        accountRepository.save(toAccount); 
+    }
 }
