0e8f3a8a977f2ab6460cc8c497fe3d390b686ffb
Caleb Powell <calebpowell@zipcoders-MacBook-Pro-3.local>
2019-07-16 14:27:49 -0400

    .git ignore fix-up

--- money-app/src/app/acc-transfer/acc-transfer.component.ts
+++ money-app/src/app/acc-transfer/acc-transfer.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Account } from '../model/account';
-import { ACCOUNTS } from '../mock-accounts';
+// import { ACCOUNTS } from '../mock-accounts';
 import { AccountService } from '../account.service';
 
 @Component({
