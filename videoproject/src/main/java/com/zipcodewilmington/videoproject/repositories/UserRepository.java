8d849321171628d8746a17a801f23746addee13b
Connor Jones <ConnorLeeJones@gmail.com>
2019-08-08 21:29:05 -0400

    Clean up

--- videoproject/src/main/java/com/zipcodewilmington/videoproject/repositories/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.zipcodewilmington.videoproject.repositories;
-
-import com.zipcodewilmington.videoproject.models.User;
-import org.springframework.data.repository.CrudRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface UserRepository extends CrudRepository<User, String> {
-}
