0814c42b691df57467c17de44073a28b4eba88a3
Han-Lin-zc <hlin04@gmail.com>
2020-04-02 15:15:39 -0400

    Finish Comment class controller and service

--- src/main/java/com/videolibrary/zipcode/fullstackapp/models/Comment.java
+++ src/main/java/com/videolibrary/zipcode/fullstackapp/models/Comment.java
@@ -1,4 +1,35 @@
 package com.videolibrary.zipcode.fullstackapp.models;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long id;
+    private String comment;
+
+    public Comment() {}
+
+    public Comment(String comment) { this.comment = comment; }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
 }
