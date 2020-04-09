42205a5abaa33a8d5bd79ceba91f2689e564b864
Giles Bradford <gbradford@Gbrads-Mac.local>
2020-04-08 20:18:16 -0400

    test case passes

--- src/test/java/com/videolibrary/zipcode/fullstackapp/Controllers/TestUserController.java
+++ src/test/java/com/videolibrary/zipcode/fullstackapp/Controllers/TestUserController.java
@@ -2,7 +2,7 @@ package com.videolibrary.zipcode.fullstackapp.Controllers;
 
 import com.videolibrary.zipcode.fullstackapp.models.User;
 import com.videolibrary.zipcode.fullstackapp.services.UserService;
-import org.junit.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,8 +13,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import java.util.Optional;
-import com.fasterxml.jackson.databind.*;
 
+import static org.mockito.ArgumentMatchers.any;
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.doReturn;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
@@ -32,25 +32,26 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TestUserController {
 
     /*
+     * This is used to represent an object or mock an object.
+     * We will make a mock object that stands in the place of service.
+     **/
+    @MockBean
+    private UserService userService;
+
+    /*
      * This mockMvc is going to act as our client. You can think of it as a webpage.
      * This will pretend to do a web request. This mockMvc will talk to the controller.
      * */
     @Autowired
     private MockMvc mockMvc;
 
-    /*
-    * This is used to represent an object or mock an object.
-    * We will make a mock object that stands in the place of service.
-    **/
-    @MockBean
-    private UserService mockUserService;
-
     @Test
     @DisplayName("GET /User/1 - Found") // This will be the name of the test.
     public void testGetUserById() throws Exception {
         // Setup the mocked service
         User mockUser = new User(1L, "Winston", "The Corgi");
-        doReturn(Optional.of(mockUser)).when(mockUserService).show(1L);
+        doReturn(mockUser).when(userService).create(mockUser);
+        doReturn(Optional.of(mockUser)).when(userService).show(1L);
 
         // Execute the GET request
         mockMvc.perform(get("/User/{id}", 1))
@@ -60,7 +61,7 @@ public class TestUserController {
                 .andExpect(content().contentType(MediaType.APPLICATION_JSON))
 
                 // Validate the returned fields
-                .andExpect(jsonPath("$.id", is(1L)))
+                .andExpect(jsonPath("$.id", is(1)))
                 .andExpect(jsonPath("$.firstName", is("Winston")))
                 .andExpect(jsonPath("$.lastName", is("The Corgi")));
 
